4ccbe04800d3a660d3dad0e69f2002e2028aa227
EmperorBob7 <davinderpaltoor@gmail.com>
2021-11-05 18:41:51 -0500

    Redirect on server creation fixed

--- public/scripts.js
+++ public/scripts.js
@@ -2,16 +2,17 @@ async function create() {
     let name = prompt("Enter a room name");
     const options = {
         method: 'POST',
-        body: JSON.stringify({"name": name}),
+        body: JSON.stringify({ "name": name }),
         headers: {
             'Content-Type': 'application/json'
         }
     }
     let results = await fetch("/create", options);
-    let res = await results.json();
-    if(res.success) {
+    let res = JSON.parse(await results.json());
+    if(res.success)
         window.location.href = "/servers/" + name;
-    }
+    else
+        alert("This name is currently taken.");
 }
 async function join() {
     let name = prompt("Enter Room Code");
