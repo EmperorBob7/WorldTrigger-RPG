fd726343d05708bb19ebc8b3a4f4d4a9bbb75d3c
EmperorBob7 <davinderpaltoor@gmail.com>
2021-11-05 17:31:02 -0500

    NPM Init

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
